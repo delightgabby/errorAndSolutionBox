435d01947074bb5d1e16e59955075ddd6a0e2b86
delightgabby <USER@DESKTOP-VTQCVES>
2022-03-21 15:41:31 +0100

    3SolutionUpdate

--- errorAndSolution.js
+++ errorAndSolution.js
@@ -1,3 +1,4 @@
+// GIT & GITHUB ERROR AND SOLUTION
 // when you duplicate an already pushed file to git, first make sure you view the hidden .git file then you delete  it so you can be able to push the new copy back to gitHub.
 // when you are having issue in pushing you can use git push -u +master to force push.
 // when you can't commit after downloading a new git or on a new system and you do git commit -m "first commit" but is did not work, you do git --config user.name follow by your username or e-mail.
